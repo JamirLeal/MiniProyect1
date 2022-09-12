ca954fb16cac627071eb50dc1a105da9a392cf4b
JamirLeal <a00826275@itesm.mx>
2022-09-12 14:44:37 -0500

    Setup of cmake devel env.

---
+++ build/CMakeFiles/phonebook.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/phonebook.dir/main.c.o"
+  "CMakeFiles/phonebook.dir/main.c.o.d"
+  "phonebook"
+  "phonebook.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/phonebook.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
