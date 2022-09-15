3aebf50835bff73153955c581d081411b5f63559
JamirLeal <a00826275@itesm.mx>
2022-09-15 14:06:29 -0600

    Operations are working without password.

--- main.c
+++ main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int getOperationCode(char *operation) {
+    if (operation == "Add") {
+        return 1;
+    } else if (operation == "Edit") {
+        return 2;
+    } else if (operation == "Search") {
+        return 3;
+    } else if (operation == "Delete") {
+        return 4;
+    }
+}
+
 int main() {
-    printf("Hello world\n");
+    FILE *fp = fopen("phonebook.dat", "ab+");
+
+    // add, search, edit, delete
+    printf("Write the operation type that you want to do:\nAdd\nSearch\nEdit\nDelete\n");   
+
+    char *operation;
+
+    scanf("%s", operation);
+
+    switch (operation) {
+        case "Add":
+            continue;
+        case ""
+    }
+
 }
